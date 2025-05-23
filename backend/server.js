75921217053ca85da733c25e839f16e1a7d33ed1
RAKESH LAHA <163835738+Rakesh7679@users.noreply.github.com>
2025-05-23 20:58:34 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import cors from 'cors';
+// import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js'
+import dotenv from 'dotenv';
+import doctorRouter from './routes/doctorRoute.js';
+import userRouter from './routes/userRoute.js';
+
+dotenv.config();
+
+
+
+
+
+//app config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB()
+connectCloudinary()
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+//api endpoint
+app.use('/api/admin', adminRouter)
+app.use('/api/doctor', doctorRouter)
+app.use('/api/user', userRouter)
+
+
+//localhost:4000/api/admin/add-doctor
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+//listening to server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
